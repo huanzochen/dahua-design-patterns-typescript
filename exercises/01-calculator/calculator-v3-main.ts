c1a2362f93e131257649ef70ed8ea217195197a9
ziquanh19 <cy94295@gmail.com>
2026-09-20 14:16:45 +0800

    add

--- exercises/01-calculator/calculator-v3-main.ts
+++ exercises/01-calculator/calculator-v3-main.ts
@@ -1,12 +1,23 @@
-import { Operation, OperationAdd } from "./calculator-v3.js";
+import {
+  Operation,
+  OperationAdd,
+  OperationDivide,
+  OperationMultiply,
+  OperationSubtract,
+} from "./calculator-v3.js";
 
-function main(): void {
-  const operation: Operation = new OperationAdd();
-
+function calculate(operation: Operation): number {
   operation.numberA = 10;
   operation.numberB = 2;
 
-  console.log(`Result: ${operation.getResult()}`);
+  return operation.getResult();
+}
+
+function main(): void {
+  console.log(`Add: ${calculate(new OperationAdd())}`);
+  console.log(`Subtract: ${calculate(new OperationSubtract())}`);
+  console.log(`Multiply: ${calculate(new OperationMultiply())}`);
+  console.log(`Divide: ${calculate(new OperationDivide())}`);
 }
 
 main();
